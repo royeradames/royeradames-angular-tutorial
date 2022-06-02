015dfc6e8a79044283e007ecd67cd9d7fd0973b9
Royer Adames <royeraadames@gmail.com>
2022-06-02 03:04:46 -0400

    📦add previous and forward buttons

--- src/app/tutorial-template/table-of-contents/table-of-content.component.ts
+++ src/app/tutorial-template/table-of-contents/table-of-content.component.ts
@@ -12,17 +12,35 @@ export class TableOfContentComponent {
   notesNav: TableOfContentInterface[] = this.notesService.notesNav();
 
   isHidingNav = true;
+  previousTutorialLink = "";
+  nextTutorialLink = "";
 
   constructor(
     private route: ActivatedRoute,
     private notesService: NotesService
   ) {
     this.route.params.subscribe((params) => {
-      this.section =
-        this.notesNav.find((note) => {
-          const currentDomain = `/${params["title"] ? params["title"] : ""}`;
-          return note.domainPath === currentDomain;
-        })?.section || "Params title doesn't equal note link";
+      this.notesNav.forEach((note, index) => {
+        const currentDomain = `/${params["title"] ? params["title"] : ""}`;
+        const isInCurrentDomain = note.domainPath === currentDomain;
+        console.log(note.domainPath, "==", currentDomain);
+        console.log(index);
+        if (isInCurrentDomain) {
+          console.log(note.domainPath);
+          console.log(currentDomain);
+          console.log(index - 1 < 0);
+          const maxIndex = this.notesNav.length - 1;
+          console.log(index + 1 <= maxIndex);
+          this.section = note.section;
+          this.previousTutorialLink =
+            index - 1 >= 0 ? `${this.notesNav[index - 1].domainPath}` : "";
+          console.log("working");
+          this.nextTutorialLink =
+            index + 1 <= maxIndex
+              ? `${this.notesNav[index + 1].domainPath}`
+              : "";
+        }
+      });
     });
   }
 }
