1259ddf6b885223a91f9d62f3fb40fe0276916fd
Royer Adames <royeraadames@gmail.com>
2022-06-07 17:20:07 -0400

    👌remove comments

--- src/app/svg/arrow/arrow.component.ts
+++ src/app/svg/arrow/arrow.component.ts
@@ -6,13 +6,8 @@ import { Component, Input, OnInit } from "@angular/core";
   styleUrls: ["./arrow.component.scss"],
 })
 export class ArrowComponent implements OnInit {
-  // @Input() direction: "left" | "right" = "left";
   @Input() style = "";
-  constructor() {
-    // if (this.direction === "right") this.style = "transform: rotate(180deg)";
-    // if (this.direction === "right") this.style = "transform: scale(-1,1)";
-    // this.style = "";
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
