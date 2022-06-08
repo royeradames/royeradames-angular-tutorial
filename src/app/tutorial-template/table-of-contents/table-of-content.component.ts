8bffcc973228a0c41807828a68d0f9f40134fb08
Royer Adames <royeraadames@gmail.com>
2022-06-07 21:10:04 -0400

    📦move domain tracking

--- src/app/tutorial-template/table-of-contents/table-of-content.component.ts
+++ src/app/tutorial-template/table-of-contents/table-of-content.component.ts
@@ -1,24 +1,10 @@
 import { Component } from "@angular/core";
-import { ActivatedRoute, Params, Router } from "@angular/router";
-import { Observable } from "rxjs/internal/Observable";
-import { TutorialInterface, TutorialsOptgroup } from "src/app/data/tutorial";
+import { Router } from "@angular/router";
 import {
   NotesService,
   TableOfContentInterface,
 } from "src/app/tutorial-template/notes.service";
 
-interface trackDomainsGroupInterface {
-  meta: {
-    previousTutorialLink: string;
-    currentTutorialLink: string;
-    nextTutorialLink: string;
-  };
-  params: Params;
-  note: TutorialInterface;
-  index: number;
-  options: TutorialInterface[];
-  tutorials: TutorialsOptgroup[];
-}
 @Component({
   selector: "app-table-of-content",
   templateUrl: "./table-of-content.component.html",
@@ -33,75 +19,12 @@ export class TableOfContentComponent {
 
   meta = this.notesService.meta;
 
-  constructor(
-    private activatedRoute: ActivatedRoute,
-    private notesService: NotesService,
-    private router: Router
-  ) {
-    this.checkCurrentDomain(
-      this.meta,
-      this.activatedRoute.params,
-      this.notesNavOptgroup,
-      this.trackDomainsGroup
-    );
-  }
+  constructor(private notesService: NotesService, private router: Router) {}
 
   navigateTo(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     if (filterValue) {
       this.router.navigate([filterValue]);
-    }
-  }
-
-  private checkCurrentDomain(
-    meta: typeof this.meta,
-    params: Observable<Params>,
-    tutorials: TutorialsOptgroup[],
-    trackDomain: ({
-      options,
-      params,
-      note,
-      index,
-    }: trackDomainsGroupInterface) => void
-  ): void {
-    params.subscribe((params) => {
-      tutorials.forEach((group) =>
-        group.options.forEach((tutorial, index) =>
-          trackDomain({
-            meta,
-            tutorials,
-            options: group.options,
-            params,
-            note: tutorial,
-            index,
-          })
-        )
-      );
-    });
-  }
-
-  private trackDomainsGroup({
-    meta,
-    tutorials,
-    options,
-    params,
-    note,
-    index,
-  }: trackDomainsGroupInterface): void {
-    const currentDomain = params["title"] || "";
-    const isInCurrentDomain = note.domainPath === currentDomain;
-    if (isInCurrentDomain) {
-      const maxOptionsIndex = options.length - 1;
-      const maxGroupIndex = tutorials.length - 1;
-
-      meta.previousTutorialLink =
-        index - 1 >= 0 ? `${options[index - 1].domainPath}` : "";
-
-      meta.currentTutorialLink =
-        index - 1 >= 0 ? `${options[index].domainPath}` : "";
-
-      meta.nextTutorialLink =
-        index + 1 <= maxOptionsIndex ? `${options[index + 1].domainPath}` : "";
     }
   }
 }
--- src/app/tutorial-template/tutorial-template.component.ts
+++ src/app/tutorial-template/tutorial-template.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
-import { ActivatedRoute, Router } from "@angular/router";
-import { TutorialInterface } from "../data/tutorial";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Observable } from "rxjs/internal/Observable";
+import { TutorialInterface, TutorialsOptgroup } from "../data/tutorial";
 import { NotesService } from "./notes.service";
+
+export interface trackDomainsGroupInterface {
+  meta: {
+    previousTutorialLink: string;
+    currentTutorialLink: string;
+    nextTutorialLink: string;
+  };
+  params: Params;
+  note: TutorialInterface;
+  index: number;
+  options: TutorialInterface[];
+  tutorials: TutorialsOptgroup[];
+}
 
 export interface MetaInterface
   extends Omit<TutorialInterface, "aPath" | "bPath"> {
@@ -19,25 +33,33 @@ export interface MetaInterface
   styleUrls: ["./tutorial-template.component.scss"],
 })
 export class TutorialTemplateComponent {
-  meta: MetaInterface;
-  playgroundExist: boolean;
+  meta = this.getMeta(this.notesService.notes);
+  playgroundExist = this.meta.aPath.toString().length > 74;
+
+  notesNavOptgroup = this.notesService.notesOptgroups;
+
+  notesServiceMeta = this.notesService.meta;
 
   constructor(
     private sanitizer: DomSanitizer,
     private router: Router,
-    private route: ActivatedRoute,
+    private activatedRoute: ActivatedRoute,
     private notesService: NotesService
   ) {
-    this.meta = this.getMeta(this.notesService.notes);
+    // when redirected to home remove redirected path
+    if (this.meta["domainPath"] === "") this.router.navigate(["/"]);
+
     this.router.events.subscribe(() => {
       this.meta = this.getMeta(this.notesService.notes);
       this.playgroundExist = this.meta.aPath.toString().length > 74;
     });
 
-    // when redirected to home remove redirected path
-    if (this.meta["domainPath"] === "") this.router.navigate(["/"]);
-
-    this.playgroundExist = this.meta.aPath.toString().length > 74;
+    this.trackDomainChange(
+      this.notesServiceMeta,
+      this.activatedRoute.params,
+      this.notesNavOptgroup,
+      this.setNotesServiceMeta
+    );
   }
 
   showReset(): void {
@@ -56,7 +78,7 @@ export class TutorialTemplateComponent {
   }
 
   private loadNote(notes: TutorialInterface[]): TutorialInterface {
-    const currentDomainPath = this.route.snapshot.params["title"];
+    const currentDomainPath = this.activatedRoute.snapshot.params["title"];
     const note = notes.find((note) => note.domainPath === currentDomainPath);
     if (note === undefined) return notes[0];
     return note;
@@ -76,5 +98,57 @@ export class TutorialTemplateComponent {
       resetText: "Reset",
       currentText: "Show me",
     };
+  }
+
+  private trackDomainChange(
+    meta: typeof this.notesServiceMeta,
+    params: Observable<Params>,
+    tutorials: TutorialsOptgroup[],
+    trackDomain: ({
+      options,
+      params,
+      note,
+      index,
+    }: trackDomainsGroupInterface) => void
+  ): void {
+    params.subscribe((params) => {
+      tutorials.forEach((group) =>
+        group.options.forEach((tutorial, index) =>
+          trackDomain({
+            meta,
+            tutorials,
+            options: group.options,
+            params,
+            note: tutorial,
+            index,
+          })
+        )
+      );
+    });
+  }
+
+  private setNotesServiceMeta({
+    meta,
+    tutorials,
+    options,
+    params,
+    note,
+    index,
+  }: trackDomainsGroupInterface): void {
+    const currentDomain = params["title"] || "";
+    const isInCurrentDomain = note.domainPath === currentDomain;
+    if (isInCurrentDomain) {
+      const maxOptionsIndex = options.length - 1;
+      const maxGroupIndex = tutorials.length - 1;
+
+      meta.previousTutorialLink =
+        index - 1 >= 0 ? `${options[index - 1].domainPath}` : "";
+
+      meta.currentTutorialLink =
+        index - 1 >= 0 ? `${options[index].domainPath}` : "";
+
+      meta.nextTutorialLink =
+        index + 1 <= maxOptionsIndex ? `${options[index + 1].domainPath}` : "";
+    }
   }
 }
